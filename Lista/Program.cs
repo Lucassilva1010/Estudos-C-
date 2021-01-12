8a55d1bf80235310ee6e67d6683fa64ba2e5e159
lucassilva1010 <lucasaugusto.araripina@gmail.com>
2021-01-12 18:42:35 -0300

    Novo Implemento (Lista)

---
+++ Lista/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Criando uma Lista de String no C#
+            /// Criar uma lista em C#, sempre é preciso ADD a DLL de nome: Colletions..Generic;
+
+            List<String> ss = new List<string>();
+            // insereindo valores na Lista
+            ss.Add("lucas");// adicionando valores a cara posição da lista;
+            ss.Add("Antonella");
+            ss.Add("Anastacia");
+
+           // Por que não se pode Imprimir a variavel de referencia direta da Lista? 
+
+
+            foreach(String c in ss)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.Read();
+        }
+    }
+}
