8df936e85771d11a30c5fb178df2bf9fe1bd9998
lucassilva1010 <lucasaugusto.araripina@gmail.com>
2021-01-13 08:17:23 -0300

    Novas Atualizações no While , NotasAlunos, RecusivaAlunos

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -10,19 +10,38 @@ namespace Lista
             /// Criando uma Lista de String no C#
             /// Criar uma lista em C#, sempre é preciso ADD a DLL de nome: Colletions..Generic;
 
-            List<String> ss = new List<string>();
+
+            // O uso da da String com "S", será um "Objeto String" e não uma Variavel
+            List<string> ss = new List<string>(); 
+            
             // insereindo valores na Lista
-            ss.Add("lucas");// adicionando valores a cara posição da lista;
+            ss.Add("Lucas");// adicionando valores a cara posição da lista;
             ss.Add("Antonella");
             ss.Add("Anastacia");
+            
+            int i = 0;
 
-           // Por que não se pode Imprimir a variavel de referencia direta da Lista? 
+            do
+            { // colocando em looping a toda a operação se caso o nome do Indice
+              // forverdadeiro;
+                Console.WriteLine("......");
 
-
-            foreach(String c in ss)
-            {
-                Console.WriteLine(c);
-            }
+                foreach (string c in ss)
+                {
+                    if ((c == "Lucas") || (c == "Antonella") || (c == "Anastacia"))// Buscando nome inserido na Lista, e acionando comando;
+                    {
+                        Console.WriteLine("Nomes Aceito!" + c);
+                                          }
+                    else
+                    {
+                        Console.WriteLine("Nome não encontrado, ou digitação errada!");
+                        Console.WriteLine("Repita a Operação");
+                    }
+                    // Console.WriteLine(c);
+                }
+                i++;
+                Console.WriteLine("Familia formada!");
+            } while (i < 1);
 
             Console.Read();
         }
---
+++ Recursiva-NotasAluno/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Recursiva_NotasAluno
+{
+    class Program
+    {
+                    
+        static void Main(string[] args)
+        {
+            
+            string s;
+
+            do
+            {
+
+                Console.WriteLine("Bemvindo ao programa de Notas de Aluno\n");
+                notas();
+
+                Console.WriteLine("\n\nDeseja continuar testando?");
+                s = Console.ReadLine();
+
+            } while ((s != "n") && (s != "não"));
+        }
+
+        // Toda a Esturura, usando a variavel sendo Inicializada
+        //   string s = ""; 
+        //    while((s !="n")&& (s != "não"))
+        //    {
+
+        //    Console.WriteLine("Bemvindo ao programa de Notas de Aluno\n");
+        //    notas();
+
+        //        Console.WriteLine("\n\nDeseja continuar testando?");
+        //        s = Console.ReadLine();
+        //    }
+        //}
+
+
+        public static void notas()
+        {
+            string nome, s;
+            int n1, n2, n3, n4;
+            double med;
+            Console.WriteLine("Digite o Nome do aluno!");
+            nome = Console.ReadLine();
+            Console.WriteLine("   ");
+            Console.WriteLine("   ");
+            Console.WriteLine("Seja bem Vindo!" + nome);
+            Console.Clear();
+
+            Console.WriteLine("Obrigado!");
+            Console.Clear();
+            Console.WriteLine("   ");
+
+            Console.WriteLine("Digite a Nota 01 de:" + nome);
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a Nota 02 de:" + nome);
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a Nota 03 de:" + nome);
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a Nota 04 de:" + nome);
+            n4 = Convert.ToInt32(Console.ReadLine());
+            med = (n1 + n2 + n3 + n4) / 2;
+
+            if (med >= 7)
+            {
+
+                Console.WriteLine("Parabéns," + nome + " Sua media foi de: " + med);
+                Console.WriteLine("Parabéns," + nome + " Você foi Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Melhore seus estudos! " + nome);
+                Console.Clear();
+            }
+        }
+
+
+    }
+}
