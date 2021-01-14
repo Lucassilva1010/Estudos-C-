710696a17c1afdd5fe3f86d4dd4bd89d11237744
lucassilva1010 <lucasaugusto.araripina@gmail.com>
2021-01-13 23:04:50 -0300

    Recursiva Complexa

--- Array/Program.cs
+++ Array/Program.cs
@@ -13,12 +13,13 @@ namespace Array
             var nome = new string[5];// Dinamica
 
             // Setando o meu Arry
-            for (int i = 0; i < ss.Length; i++)
+            for (int i = 0; i < ss.Length; i++)// Adicionando valor ao meu Array
+                //o valor de "SS", é passado para Length(analisador de possição)
+                //O laço vai se repetir até o valor total o Indice do Array
             {
                 Console.WriteLine("Digite o Nome: \n");
                 ss[i] = Console.ReadLine();
             }
-           
           
            foreach(string most in  ss)
             {
